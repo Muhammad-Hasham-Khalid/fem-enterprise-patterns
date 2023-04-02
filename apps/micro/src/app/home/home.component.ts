1a826bd7476606332daee29ae99858d4662e26e9
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2023-04-02 20:12:57 +0500

    refactor: widgets reducer

--- apps/micro/src/app/home/home.component.ts
+++ apps/micro/src/app/home/home.component.ts
@@ -9,26 +9,43 @@ import { v4 as uuidv4 } from 'uuid';
 })
 export class HomeComponent {
   price;
-  mode;
   widgets: Widget[];
 
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
-      case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
-        break;
-      case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
-        break;
-      case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
-        break;
-      default:
-        break;
-    }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
+  reCalculateTotal(mode: string, widgets: Widget[], widget: Widget) {
+    this.widgets = this.updateWidgets(mode, widgets, widget);
+    this.price = this.getTotalPrice(this.widgets);
   }
 
+  updateWidgets(mode: string, widgets: Widget[], widget: Widget) {
+    switch (mode) {
+      case 'create':
+        return this.addWidget(widgets, widget);
+      case 'update':
+        return this.updateWidget(widgets, widget);
+      case 'delete':
+        return this.deleteWidget(widgets, widget);
+      default:
+        return widgets;
+    }
+  }
+
+  getTotalPrice(widgets: Widget[]) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+
+  addWidget(widgets: Widget[], widget: Widget) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    return [newWidget, ...widgets];
+  }
+
+  updateWidget(widgets: Widget[], widget: Widget) {
+    return widgets.map((_widget) => {
+      if (_widget.id === widget.id) return widget;
+      return _widget;
+    });
+  }
+
+  deleteWidget(widgets: Widget[], widget: Widget) {
+    return widgets.filter((_widget) => _widget.id !== widget.id);
+  }
 }
