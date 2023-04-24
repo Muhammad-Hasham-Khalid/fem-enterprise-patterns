3052591b47dcf85c4ed8be3f1620f60adc9d0548
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2023-04-24 12:09:21 +0500

    add object modelling

--- apps/meso/src/app/home/home.component.ts
+++ apps/meso/src/app/home/home.component.ts
@@ -1,6 +1,98 @@
 import { Component, OnInit } from '@angular/core';
 
-const tango = [];
+interface BaseEntity {
+  id: string | null;
+}
+
+interface Client extends BaseEntity {
+  firstName: string;
+  lastName: string;
+  company: string;
+}
+
+const peter: Client = {
+  id: '1',
+  firstName: 'Peter',
+  lastName: 'Parker',
+  company: 'Acme, Inc.',
+};
+
+const john: Client = {
+  id: '2',
+  firstName: 'John',
+  lastName: 'Doe',
+  company: 'NA',
+};
+
+const clients: Client[] = [peter, john];
+
+interface ClientsState {
+  clients: Client[];
+  currentClient: Client;
+}
+
+const newClient: Client = {
+  id: null,
+  firstName: '',
+  lastName: '',
+  company: '',
+};
+
+const initialState: ClientsState = {
+  clients,
+  currentClient: newClient,
+};
+
+interface Project extends BaseEntity {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface ProjectsState {
+  projects: Project[];
+  currentProject: Project;
+}
+
+const superProject: Project = {
+  id: '1',
+  title: 'Super Project',
+  description: 'This is awesome!',
+  completed: false,
+};
+
+const boringProject: Project = {
+  id: '2',
+  title: 'Boring Project',
+  description: 'This is boring!',
+  completed: true,
+};
+
+const projects: Project[] = [superProject, boringProject];
+
+const newProject: Project = {
+  id: null,
+  title: '',
+  description: '',
+  completed: false,
+};
+
+const initialProjectState: ProjectsState = {
+  projects,
+  currentProject: newProject,
+};
+
+interface AppState {
+  clientsState: ClientsState;
+  projectsState: ProjectsState;
+}
+
+const appState: AppState = {
+  clientsState: initialState,
+  projectsState: initialProjectState,
+};
+
+const tango = appState;
 
 @Component({
   selector: 'fem-home',
